7244e7a0da92be315522cb6eba25729b763721f9
Adib Fairuz Ikbar <fairuzadib@gmail.com>
2022-07-08 17:21:47 +0700

    add device size info

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,20 @@
 	import { Router, Route } from "svelte-navigator";
 	import Home from './pages/Home.svelte';
 	import Search from './pages/Search.svelte';
+
+	let isDesktop = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) > 920;
+
 </script>
 
 <main>
-	<Router>
-		<Route path="/" component={Home} let:location />
-		<Route path="/search" component={Search} let:location />
-	</Router>
+	{#if isDesktop}
+		<Router>
+			<Route path="/" component={Home} let:location />
+			<Route path="/search" component={Search} let:location />
+		</Router>
+	{:else}
+		<div>Only support the desktop size</div>
+	{/if}
 </main>
 
 <style lang="postcss" global>
